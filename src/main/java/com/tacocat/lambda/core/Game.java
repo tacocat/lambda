61cf2afa3ca063802fea2d280f3d9c227ef25c5d
tacocat <musson1@illinois.edu>
2017-04-02 19:24:06 -0500

    New way of setting initial game state

--- src/main/java/com/tacocat/lambda/core/Game.java
+++ src/main/java/com/tacocat/lambda/core/Game.java
@@ -1,24 +1,20 @@
 package com.tacocat.lambda.core;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.BiConsumer;
-
-import com.tacocat.lambda.core.component.Component;
 import com.tacocat.lambda.core.component.ComponentStore;
 import com.tacocat.lambda.core.platform.Platform;
+import com.tacocat.lambda.core.entity.Entity;
 import com.tacocat.lambda.core.system.GameSystem;
 
 public class Game {
-    private List<GameSystem> systems;
+    private GameList<GameSystem> systems;
     private ComponentStore components;
     private GameEngine gameEngine;
     private RenderEngine renderEngine;
     private Window window;
 
+
     public Game(Platform platform) {
-        systems = new ArrayList<GameSystem>();
+        systems = new GameList<>();
         components = new ComponentStore();
         gameEngine = new GameEngine();
 
@@ -26,16 +22,27 @@ public class Game {
         this.window = platform.getWindow();
     }
 
-    public void init(BiConsumer<List<GameSystem>, ComponentStore> initFunction) {
-        initFunction.accept(systems, components);
-    }
-
-    public void useSystems(GameSystem ...newSystems) {
-        Collections.addAll(systems, newSystems);
-    }
-
-    public void addComponent(Component component) {
-        components.add(component);
+    /**
+     * Programmatically register GameSystems and define the game's starting state
+     *
+     * <pre>
+     * game.init((systems, entities, components) -> {
+     *     systems.add(
+     *         new CombatSystem()
+     *     );
+     *
+     *     entities.add(
+     *         new Entity(new Health(100), new Damage(25)),
+     *         new Entity(new Health(150), new Damage(15)
+     *     );
+     * }
+     * </pre>
+     * @param initFunction
+     */
+    public void init(InitFunction<GameList<GameSystem>, GameList<Entity>, ComponentStore> initFunction) {
+        GameList<Entity> entities = new GameList<>();
+        initFunction.apply(systems, entities, components);
+        entities.forEach(components::registerEntity);
     }
 
     public void run() {
@@ -47,5 +54,24 @@ public class Game {
 
             renderEngine.render(window);
         }
+    }
+
+    /**
+     * @return GameSystems currently being used
+     */
+    public GameList<GameSystem> getSystems() {
+        return systems;
+    }
+
+    /**
+     * @return all Components in the game's current state
+     */
+    public ComponentStore getComponents() {
+        return components;
+    }
+
+    @FunctionalInterface
+    public interface InitFunction<A,B,C> {
+        void apply(A a, B b, C c);
     }
 }
